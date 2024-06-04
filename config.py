b6882712c243cfb7c12b774bf4c3c1c35c357feb
Boran-Antunna <yakasirivamsivamsivamsi7@gmail.com>
2024-06-04 14:57:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "25976580"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "b5562ab77a96e49bc9dd78cc103c6333"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "7029942301"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "Noob_Coderzzz"
+OWNER_ID = "6708929009"
+SUPPORT_GRP = "https://t.me/Mana_animez"
+UPDATE_CHNL = "https://t.me/moviez_cartoonz"
+OWNER_USERNAME = "UmmBaby"
 
